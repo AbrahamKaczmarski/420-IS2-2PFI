ff0125795b2bb568ee16588a628bcdd6f442171d
Marek K <marek.korzeniewski1996@gmail.com>
2021-11-30 15:04:37 +0100

    Endpoint replenishment

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,5 +4,10 @@ const controller = require('./api.controller')
 const router = Router()
 
 router.get('/', controller.welcome)
+router.get('/echo/:text', controller.writeText)
+router.post('/blog', controller.addBlog)
+router.get('/blog', controller.showBlog)
+
+
 
 module.exports = router
